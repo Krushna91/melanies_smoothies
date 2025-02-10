c6630d79816c3dc9a15a4878282e445c9ed69d6d
Krushna91 <108990287+Krushna91@users.noreply.github.com>
2025-02-10 15:26:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ st.write("The name on smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#ST.STOP()
+st.dataframe(data=my_dataframe, use_container_width=True)
+ST.STOP()
 
 #CONVERT THE SNOWPARK DATAFRAME TO PANDAS DATAFRAME
 pd_df = my_dataframe.to_pandas()
